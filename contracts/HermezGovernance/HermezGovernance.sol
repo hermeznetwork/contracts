5e12bb3e1a1d341fa457e009f55c6ca22b4eb7a3
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-11-24 10:21:29 +0100

    Remove addRole and removeRole

--- contracts/HermezGovernance/HermezGovernance.sol
+++ contracts/HermezGovernance/HermezGovernance.sol
@@ -24,34 +24,12 @@ contract HermezGovernance is Initializable, AccessControlUpgradeSafe {
     }
 
     /**
-     * @dev Function to grant a role to a desired account
-     * @param role it should be the keccak256 of the destination address and the signature of the function to call
-     * @param account account to grant access
-     */
-    function addRole(bytes32 role, address account) public {
-        grantRole(role, account);
-    }
-
-    /**
-     * @dev Function to revoke a role to a desired account
-     * @param role it should be the keccak256 of the destination address and the signature of the function to call
-     * @param account account to grant access
-     */
-    function removeRole(bytes32 role, address account) public {
-        revokeRole(role, account);
-    }
-
-    /**
      * @dev Function to execute a call. The msg.sender should have the role to be able to execute it
      * @param destination address to which the call will be made
      * @param value call value
      * @param data data of the call to make
      */
-    function execute(
-        address destination,
-        uint256 value,
-        bytes memory data
-    ) external {
+    function execute(address destination, bytes memory data) external {
         // Decode the signature
         bytes4 dataSignature = abi.decode(data, (bytes4));
         bytes32 role = keccak256(abi.encodePacked(destination, dataSignature));
@@ -61,7 +39,7 @@ contract HermezGovernance is Initializable, AccessControlUpgradeSafe {
         );
 
         (bool succcess, bytes memory returnData) = destination.call{
-            value: value
+            value: msg.value
         }(data);
         if (succcess) {
             emit ExecOk(returnData);
