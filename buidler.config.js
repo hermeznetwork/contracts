1bc84c39f48bd64ab3bc8335808831739ccb7249
invocamanman <jesus.ligero@yahoo.es>
2021-01-20 14:01:56 +0100

    add ropsten deployment

--- buidler.config.js
+++ buidler.config.js
@@ -64,6 +64,15 @@ module.exports = {
         count: 20,
       },
     },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+    },
   },
   etherscan: {
     // The url for the Etherscan API you want to use.
