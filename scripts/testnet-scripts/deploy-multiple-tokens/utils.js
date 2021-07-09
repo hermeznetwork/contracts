c079577824e7322f47e9ddcb7bfe2808ef07c126
invocamanman <jesus.ligero@yahoo.es>
2021-07-09 12:26:56 +0200

    script deploy multiple tokens

--- scripts/testnet-scripts/deploy-multiple-tokens/utils.js
+++ scripts/testnet-scripts/deploy-multiple-tokens/utils.js
@@ -10,11 +10,8 @@ function checkEnvVariables() {
   envVarCheck(process.env.MNEMONIC, "MNEMONIC");
   envVarCheck(process.env.INDEX, "INDEX");
   envVarCheck(process.env.INFURA_PROJECT_ID, "INFURA_PROJECT_ID");
-  envVarCheck(process.env.ETHERSCAN_API_KEY, "ETHERSCAN_API_KEY");
-  envVarCheck(process.env.TOKEN_NAME, "TOKEN_NAME");
-  envVarCheck(process.env.TOKEN_SYMBOL, "TOKEN_SYMBOL");
-  envVarCheck(process.env.TOKEN_DECIMALS, "TOKEN_DECIMALS");
-  envVarCheck(process.env.INITIAL_ACCOUNT, "INITIAL_ACCOUNT");
+  envVarCheck(process.env.HARDHAT_NETWORK, "ETHERSCAN_API_KEY");
+  envVarCheck(process.env.ETHERSCAN_API_KEY, "HARDHAT_NETWORK");
 
   return true;
 }
--- scripts/testnet-scripts/deploy-simple-token/deploy.js
+++ scripts/testnet-scripts/deploy-simple-token/deploy.js
@@ -5,96 +5,96 @@ const { ethers } = require("hardhat");
 const utils = require("./utils");
 
 async function main() {
-    // compìle contracts
-    await bre.run("compile");
+  // compìle contracts
+  await bre.run("compile");
 
-    // load Mnemonic accounts
-    const signersArray = await ethers.getSigners();
+  // load Mnemonic accounts
+  const signersArray = await ethers.getSigners();
 
-    // check environment variables
-    utils.checkEnvVariables();
+  // check environment variables
+  utils.checkEnvVariables();
 
-    const deployer = signersArray[process.env.INDEX];
-    const deployerAddress = await deployer.getAddress();
-    const deployerBalance = (await deployer.getBalance()).toString();
+  const deployer = signersArray[process.env.INDEX];
+  const deployerAddress = await deployer.getAddress();
+  const deployerBalance = (await deployer.getBalance()).toString();
 
-    console.log("<=============================>");
-    console.log("<========DEPLOYER INFO========>");
-    console.log("<=============================>")
-    console.log(`address: ${deployerAddress}`);
-    console.log(`balance: ${deployerBalance} ETH\n\n`);
+  console.log("<=============================>");
+  console.log("<========DEPLOYER INFO========>");
+  console.log("<=============================>");
+  console.log(`address: ${deployerAddress}`);
+  console.log(`balance: ${deployerBalance} ETH\n\n`);
 
-    const instanceToken = await (await ethers.getContractFactory("ERC20Custom")).connect(deployer);
+  const instanceToken = await (await ethers.getContractFactory("ERC20MockDecimals")).connect(deployer);
 
-    console.log("<=============================>");
-    console.log("<===========DEPLOY============>");
-    console.log("<=============================>");
+  console.log("<=============================>");
+  console.log("<===========DEPLOY============>");
+  console.log("<=============================>");
 
-    console.log("Info token paramneters:");
-    console.log("   tokenName: ", process.env.TOKEN_NAME);
-    console.log("   tokenSymbol: ", process.env.TOKEN_SYMBOL);
-    console.log("   tokenDecimals: ", process.env.TOKEN_DECIMALS);
-    console.log("   InitialAccount: ", process.env.INITIAL_ACCOUNT);
+  console.log("Info token paramneters:");
+  console.log("   tokenName: ", process.env.TOKEN_NAME);
+  console.log("   tokenSymbol: ", process.env.TOKEN_SYMBOL);
+  console.log("   tokenDecimals: ", process.env.TOKEN_DECIMALS);
+  console.log("   InitialAccount: ", process.env.INITIAL_ACCOUNT);
 
-    const tokenInitialAmount = ethers.BigNumber.from(
-        "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
-    );
-    console.log("Token is being deployed...");
-    const deployToken = await instanceToken.deploy(
-        process.env.TOKEN_NAME,
-        process.env.TOKEN_SYMBOL,
-        process.env.TOKEN_DECIMALS,
-        process.env.INITIAL_ACCOUNT,
-        tokenInitialAmount
-    );
-    await deployToken.deployed();
-    const deployTokenAddress = deployToken.address;
-    console.log(`Token deployed at: ${deployTokenAddress}\n\n`);
+  const tokenInitialAmount = ethers.BigNumber.from(
+    "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+  );
+  console.log("Token is being deployed...");
+  const deployToken = await instanceToken.deploy(
+    process.env.TOKEN_NAME,
+    process.env.TOKEN_SYMBOL,
+    process.env.INITIAL_ACCOUNT,
+    tokenInitialAmount,
+    process.env.TOKEN_DECIMALS,
+  );
+  await deployToken.deployed();
+  const deployTokenAddress = deployToken.address;
+  console.log(`Token deployed at: ${deployTokenAddress}\n\n`);
 
-    console.log("<=============================>");
-    console.log("<===========VERIFY============>");
-    console.log("<=============================>");
+  console.log("<=============================>");
+  console.log("<===========VERIFY============>");
+  console.log("<=============================>");
 
-    const maxRetries = 10;
-    const timeoutRetry = 60000;
-    let numRetry = 0;
-    let lastError;
-    let success = false;
+  const maxRetries = 10;
+  const timeoutRetry = 60000;
+  let numRetry = 0;
+  let lastError;
+  let success = false;
 
-    while (numRetry < maxRetries && !success) {
-        try {
-            await bre.run("verify:verify",
-                {
-                    address: deployTokenAddress,
-                    constructorArguments: [
-                        process.env.TOKEN_NAME,
-                        process.env.TOKEN_SYMBOL,
-                        process.env.TOKEN_DECIMALS,
-                        process.env.INITIAL_ACCOUNT,
-                        tokenInitialAmount
-                    ]
-                }
-            );
-            success = true;
-        } catch (error) {
-            numRetry += 1;
-            lastError = error;
-            await utils.timeout(timeoutRetry);
-            console.log(`Trying verifying contract: ${numRetry}`);
+  while (numRetry < maxRetries && !success) {
+    try {
+      await bre.run("verify:verify",
+        {
+          address: deployTokenAddress,
+          constructorArguments: [
+            process.env.TOKEN_NAME,
+            process.env.TOKEN_SYMBOL,
+            process.env.INITIAL_ACCOUNT,
+            tokenInitialAmount,
+            process.env.TOKEN_DECIMALS,
+          ]
         }
+      );
+      success = true;
+    } catch (error) {
+      numRetry += 1;
+      lastError = error;
+      await utils.timeout(timeoutRetry);
+      console.log(`Trying verifying contract: ${numRetry}`);
     }
+  }
 
-    if (success) {
-        console.log("Contract verified correctly");
-    } else {
-        console.log("Not able to verify contract");
-        console.log(lastError);
-    }
+  if (success) {
+    console.log("Contract verified correctly");
+  } else {
+    console.log("Not able to verify contract");
+    console.log(lastError);
+  }
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
